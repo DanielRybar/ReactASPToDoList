bac2c384b6a63f63fa69cbfccae16a7a2be4acb2
Daniel Rybář <u19.422@pslib.cloud>
2022-11-10 13:16:08 +0100

    Add project files.

---
+++ ReactASPToDoList/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReactASPToDoList.Models;
+
+namespace ReactASPToDoList.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Models.Task> Tasks { get; set; }
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ ReactASPToDoList/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace ReactASPToDoList.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+        public ICollection<Task> Tasks { get; set; }
+    }
+}
